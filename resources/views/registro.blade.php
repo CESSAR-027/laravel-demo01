fa26d5814195cab3d02b036a762dcf3b9c5367b2
CESSAR-027 <cesar.martinezrobles@gmail.com>
2021-01-09 19:40:32 -0500

    PROYECTO BASICO

---
+++ resources/views/registro.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>REGISTRO</h1>
+	<form action="#" method="get">
+		<div>
+			<label>USER
+				<input type="text" name="user" placeholder="ingrese nombre">
+			</label>
+		</div>
+		<div>
+			<label>PASSWORD
+				<input type="password" name="user" placeholder="ingrese contraseña">
+			</label>
+		</div>
+		
+	</form>
+</body>
+</html>
